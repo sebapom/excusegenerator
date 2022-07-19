047964d1d8b98d4f4015d93a30f88fe9aa0c2484
sebapom <sebapobletemorales@gmail.com>
2022-07-19 01:26:18 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -8,4 +8,22 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let test =
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)];
+
+  let element = document.getElementById("excuse");
+  element.innerHTML = test;
 };
